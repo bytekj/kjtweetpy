61f6a304d475bbc310dd6ca0bd1fec96b5e9533f
khaladu <kj.khaladu@gmail.com>
2014-02-24 17:24:42 +0000

    adding tweets to graph
    fixed produced errors due to miss spelt list comprehension

--- stream/tweetstream.py
+++ stream/tweetstream.py
@@ -30,36 +30,39 @@ graph = []
 
 
 def find_edge(index, tagged_tweet):
+    
     for i in range(index, len(tagged_tweet)):
         (edge,t) = tagged_tweet[i] 
-        if t.startswith('V'):
+        if type(t) == str and t.startswith('V'):
             return (i,edge)
+    return (-1,False)
 
 def get_next_node(index,tagged_tweet):
     for i in range(index, len(tagged_tweet)):
         (w,t) = tagged_tweet[i]
         if t == 'NN':
             return w
-
-
+    return False
                                  
 def node_exists(node):
     (n1,e,n2) = node
     try:
-        graph.index(n1,e,n2)
-        graph.index(n2,e,n1)
+        graph.index((n1,e,n2))
+        graph.index((n2,e,n1))
     except ValueError:
         return False
     return True
 
 def add_to_graph(tagged_tweet):
-    list_of_nodes = [(w, 'NN') for (w,t) in tagged_tweet]
+    list_of_nodes = [(w, t) for (w,t) in tagged_tweet if len(w) > 1 and t == 'NN']
     for (w,t) in list_of_nodes:
         index_NN1 = tagged_tweet.index((w,t)) +1
         (edge_index, edge) = find_edge(index_NN1,tagged_tweet)
-        next_NN = get_next_node(edge_index+1, tagged_tweet)
-        if node_exists((w,edge,next_NN)) == False:
-            graph.append((w,edge,next_NN))
+        if edge != False:
+            next_NN = get_next_node(edge_index+1, tagged_tweet)
+            if next_NN != False:
+                if node_exists((w,edge,next_NN)) == False:
+                    graph.append((w,edge,next_NN))
     return True
 
 
@@ -91,7 +94,7 @@ class StdOutListener(StreamListener):
                 print tweet_text
                 print new_tagged_sent
                 add_to_graph(new_tagged_sent)
-                print 'Accuracy: %4.1f%%' % (100.0 * unigram_tagger.evaluate(tagged_sent))
+                #print 'Accuracy: %4.1f%%' % (100.0 * unigram_tagger.evaluate(tagged_sent))
         except   UnicodeEncodeError:
             print "error in ", parsed_json 
         return True
@@ -110,5 +113,5 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
     
     #track=['costa'] is the topic of twitter data stream
-    stream.filter(track=['costa'])
+    stream.filter(track=['python'])
     #stream.sample()
